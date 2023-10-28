f2b68dc24f9ef341da8d6560726547fe615da200
Anna Cobb <64797765+a-developer-and-her-mac@users.noreply.github.com>
2023-10-28 19:49:07 +0000

    add updates

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+
+import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: "app-housing-location",
@@ -14,4 +16,6 @@ import { CommonModule } from "@angular/common";
   `,
   styleUrls: ["./housing-location.component.css"],
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  @Input() housingLocation!:HousingLocation;
+}
